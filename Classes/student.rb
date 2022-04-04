502aac8d770cb1a043ea35bdf55ee4a2fb9d4bd5
uae <aniekaudo@yahoo.com>
2022-04-04 23:54:59 +0100

    :bug: Fixing errors
    Correction base on review

--- Classes/student.rb
+++ Classes/student.rb
@@ -3,9 +3,10 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, name = 'Unknown', classroom: true)
-    super(name, age)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: true)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def play_hooky
--- Classes/teacher.rb
+++ Classes/teacher.rb
@@ -3,9 +3,10 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, name = 'Unknown', specialization: true)
-    super(name, age)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(name, age, parent_permission: true)
     @specialization = specialization
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
