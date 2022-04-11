7d6b07d4710ddcbe1c130707117f3d8762a02233
uae <aniekaudo@yahoo.com>
2022-04-11 18:33:02 +0100

    :recycle: Refactoring
    Refactored the create person, student and teacher methods

--- Classes/app.rb
+++ Classes/app.rb
@@ -46,44 +46,36 @@ class App
   def create_person
     print 'To create a student, press 1, to create a teacher, press 2 : '
     option = gets.chomp
+    
+    print 'Enter age: '
+    age = gets.chomp.to_i
+    print 'Enter name: '
+    name = gets.chomp
 
     case option
     when '1'
-      create_student
+      create_student(age, name)
     when '2'
-      create_teacher
+      create_teacher(age, name)
     else
       puts 'Invalid input. Try again'
     end
   end
 
-  def create_student
-    puts 'Create a new student'
-    print 'Enter student age: '
-    age = gets.chomp.to_i
-    print 'Enter name: '
-    name = gets.chomp
+  def create_student(age, name)
     print 'Has parent permission? [Y/N]: '
     parent_permission = gets.chomp.downcase
-    case parent_permission
-    when 'n'
-      Student.new(age, name, parent_permission: false)
-      puts 'Student doesnt have parent permission, cant rent books'
-    when 'y'
-      student = Student.new(age, name, parent_permission: false)
-      @people << student
-      puts 'Student created successfully'
-    end
+    permission = if parent_permission == 'y' then true 
+                else false
+                end
+    student = Student.new(age, name, parent_permission: permission)
+    @people << student
+    puts 'Student created successfully'
   end
 
-  def create_teacher
-    puts 'Create a new teacher'
-    print 'Enter teacher age: '
-    age = gets.chomp.to_i
+  def create_teacher(age, name)
     print 'Enter teacher specialization: '
     specialization = gets.chomp
-    print 'Enter teacher name: '
-    name = gets.chomp
     teacher = Teacher.new(age, name, specialization)
     @people << teacher
     puts 'Teacher created successfully'
