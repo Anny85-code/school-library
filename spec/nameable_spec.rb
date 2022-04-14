7eaa2f2caa903fbf268c5a02110573279857f243
uae <aniekaudo@yahoo.com>
2022-04-14 12:44:09 +0100

    :rotating_light: Fixing Compiler Issues
    Fixing offenses

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -3,9 +3,9 @@ require './Classes/nameable'
 describe Nameable do
   context 'calling a unimplemented method' do
     it 'raises' do
-      nameable = Nameable.new()
+      nameable = Nameable.new
       expect(nameable.class).to eq Nameable
       expect { nameable.correct_name }.to raise_error(NotImplementedError)
+    end
   end
-end
 end
