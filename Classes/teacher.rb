1b6a1a4ab53be0e8c22fb3990584ed26b549d4e5
orozcoding <orozcoding@gmail.com>
2022-04-14 06:50:49 -0400

    Create unit-tests for Teacher Class

--- Classes/teacher.rb
+++ Classes/teacher.rb
@@ -3,10 +3,9 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, name, specialization = 'Unknown', id = -1, parent_permission: true)
-    super(age, name, id, parent_permission: true)
+  def initialize(age, name, specialization = 'Unknown', id = -1)
+    super(age, name, id)
     @specialization = specialization
-    @parent_permission = parent_permission
   end
 
   def can_use_services?
